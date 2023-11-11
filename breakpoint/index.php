8c94213ceeccd97dde3e051426adbbb52b60cacb
nutt99 <NataGaming766@gmail.com>
2023-11-11 09:00:31 +0700

    add repo

---
+++ breakpoint/index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Breakpoints</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+  </head>
+  <body>
+  <div class="container text-center">
+  <div class="row-cols-2">
+    
+  </div>
+</div>
+    <script src="../bootstrap/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
